28cef8cb3beb65fb4c39a99b808210e17617cf7a
Hind-Bokae <eng.hens.86@gmail.com>
2026-03-12 22:55:39 +0100

    finish authentication and logout feature

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,16 @@
 import {Link} from "react-router-dom";
 import {useAuth} from "../context/AuthContext";
+import {useNavigate} from "react-router-dom";
 
 export default function Navbar() {
     const {isLoggedIn, logout, username} = useAuth();
+        const navigate = useNavigate();
+        function handleLogout() {
+            localStorage.removeItem("authToken");
+            localStorage.removeItem("username");
+            logout();
+            navigate("/login");
+        }
     return (
         <nav style={{
             padding: "10px",
@@ -23,7 +31,7 @@ export default function Navbar() {
                 <>
                 <span style={{marginLeft:"15px" , marginRight:"115px"}}>Welcome {username}</span>
 
-                <button onClick={logout}>Logout</button>
+                <button onClick={handleLogout}>Logout</button>
                 </>
              )}
         </nav>
